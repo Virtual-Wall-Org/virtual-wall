1dd2a7aab974c78348abb208e9e85b3374896a71
JC Plessis <jcplessis@gmail.com>
2020-04-08 10:58:06 +0200

    Add CICD Stack

--- bin/virtual-wall.ts
+++ bin/virtual-wall.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { VirtualWallStack } from '../lib/virtual-wall-stack';
+import { VirtualWallCICDStack } from '../lib/virtual-wall-cicd-stack';
 
 const app = new cdk.App();
 new VirtualWallStack(app, 'VirtualWallStack');
+new VirtualWallCICDStack(app, 'VirtualWallCICDStack');
---
+++ lib/virtual-wall-cicd-stack.ts
@@ -0,0 +1,45 @@
+import * as cdk from '@aws-cdk/core';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+
+export class VirtualWallCICDStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const sourceOutput = new codepipeline.Artifact("VirtualWall-Source");
+    const oauthToken = cdk.SecretValue.secretsManager('virtual-wall-secrets/github/token', { jsonField: 'github-token' });
+    const project = new codebuild.PipelineProject(this, 'VirtualWallBuild');
+
+    const sourceAction = new codepipeline_actions.GitHubSourceAction({
+      actionName: 'GitHub_Source',
+      owner: "Virtual-Wall-Org",
+      repo: "virtual-wall-front",
+      branch: 'master',
+      oauthToken: oauthToken,
+      output: sourceOutput
+    });
+
+    new codepipeline.Pipeline(this, 'VirtualWall-Pipeline', {
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [
+            sourceAction
+          ],
+        },
+        {
+          stageName: 'Build',
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'CodeBuild',
+              project,
+              input: sourceOutput,
+              outputs: [new codepipeline.Artifact()], // optional
+            })
+          ],
+        },
+      ],
+    });
+  }
+}
