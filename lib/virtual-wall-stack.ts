9bc83c3a660ce3637631315bb433dca1d4829d03
Adrien Henriet <adrien.henriet@gmail.com>
2020-04-23 00:35:44 +0200

    Draw on a canvas

--- lib/virtual-wall-stack.ts
+++ lib/virtual-wall-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from "@aws-cdk/aws-s3";
 import { CfnOutput } from '@aws-cdk/core';
+import { CloudFrontWebDistribution } from '@aws-cdk/aws-cloudfront';
+>>>>>>> Stashed changes
 
 export class VirtualWallStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,5 +13,19 @@ export class VirtualWallStack extends cdk.Stack {
     new CfnOutput(this, "bucketName", {
       value: siteBucket.bucketName
     })
+    const sourceBucketProd = new Bucket(this, 'production');
+    const sourceBucketTest = new Bucket(this, 'test');
+
+    const distribution = new CloudFrontWebDistribution(this, 'MyDistribution', {
+    originConfigs: [
+        {
+            s3OriginSource: {
+                s3BucketSource: sourceBucketProd
+            },
+            behaviors : [ {isDefaultBehavior: true}]
+        }
+    ]
+  });
+
   }
 }
