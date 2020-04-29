58badf3bc63059d95bba812775b9f4608c6168a8
JC Plessis <jcplessis@gmail.com>
2020-04-29 14:56:03 +0200

    #1 Unit test : check a bit more the code build

--- test/virtual-wall-cicd.test.ts
+++ test/virtual-wall-cicd.test.ts
@@ -1,15 +1,45 @@
-import { expect as expectCDK, countResources, haveOutput } from '@aws-cdk/assert';
+import { expect as expectCDK, countResources, haveResourceLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import VirtualWall = require('../lib/virtual-wall-cicd-stack');
 
 const app = new cdk.App();
 const stack = new VirtualWall.VirtualWallCICDStack(app, 'MyTestStack');
 test('Has two S3 Bucket for CICD', () => {
-    // THEN
     expectCDK(stack).to(countResources('AWS::S3::Bucket', 2));
 });
 
 test('Has two CodeBuild Project (CICD and Application)', () => {
-    // THEN
     expectCDK(stack).to(countResources('AWS::CodeBuild::Project', 2));
+
+    expectCDK(stack).to(haveResourceLike('AWS::CodeBuild::Project', {
+        "Artifacts": {
+            "Type": "CODEPIPELINE"
+        },
+        "Environment": {
+            "ComputeType": "BUILD_GENERAL1_SMALL",
+            "Image": "aws/codebuild/standard:2.0",
+            "PrivilegedMode": false,
+            "Type": "LINUX_CONTAINER"
+        },
+        "Source": {
+            "BuildSpec": "buildspec-cdk.yml",
+            "Type": "CODEPIPELINE"
+        },
+    }));
+
+    expectCDK(stack).to(haveResourceLike('AWS::CodeBuild::Project', {
+        "Artifacts": {
+            "Type": "CODEPIPELINE"
+        },
+        "Environment": {
+            "ComputeType": "BUILD_GENERAL1_SMALL",
+            "Image": "aws/codebuild/standard:2.0",
+            "PrivilegedMode": false,
+            "Type": "LINUX_CONTAINER"
+        },
+        "Source": {
+            "BuildSpec": "buildspec.yml",
+            "Type": "CODEPIPELINE"
+        },
+    }));
 });
