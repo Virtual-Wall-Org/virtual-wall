796429f8f4125b5ab759fca3f20f1ff6572faed9
JC Plessis <jcplessis@gmail.com>
2020-05-06 14:16:06 +0200

    Revert "Cloud formation thinks its a new API :("
    This reverts commit aea84c92f4e4ee824d5c7b9358122abe06ea4324.

--- lib/virtual-wall-stack.ts
+++ lib/virtual-wall-stack.ts
@@ -28,7 +28,6 @@ export class VirtualWallStack extends cdk.Stack {
       requestTemplates: { "application/json": '{ "statusCode": "200" }' }
     });
 
-    api.root.addMethod("GET", HelloWorldIntegration);
     api.root.addResource("api").addMethod("GET", HelloWorldIntegration);
 
     this.declareSite(api);
